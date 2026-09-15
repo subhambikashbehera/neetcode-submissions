f4d112f6babd0428619e1503d0263a61e78d4d60
Subham Bikash <69251887+subhambikashbehera@users.noreply.github.com>
2026-09-15 10:58:10 +0530

    Add: longest-substring-without-duplicates - submission-4

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-4.py
@@ -0,0 +1,32 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        # maxS = 0
+        
+        # for i in range(len(s)):
+        #     seen = set()
+        #     for j in range(i,len(s)):
+        #         if s[j] not in seen:
+        #             seen.add(s[j])
+        #             maxS = max(maxS,len(seen))
+        #         else:
+        #             break
+        # return maxS          
+
+        # we will be doing with sliding window now
+        left = 0
+        maxS = 0
+        seen = set()
+        for right in range(len(s)):
+            while s[right] in seen:
+                seen.remove(s[left])
+                left+=1
+            seen.add(s[right])
+            maxS = max(maxS,len(seen))
+        return maxS        
+
+
+
+
+
+
+        
